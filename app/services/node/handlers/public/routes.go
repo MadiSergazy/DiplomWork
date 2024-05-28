30cf4299dd8b32fdbc4a252ddd626b974260a74d
Madi <saitamenter@gmail.com>
2024-05-28 17:09:59 +0500

    final version

---
+++ app/services/node/handlers/public/routes.go
@@ -0,0 +1,44 @@
+package public
+
+import (
+	"diplom/foundation/blockchain/state"
+	"diplom/foundation/events"
+	"diplom/foundation/nameservice"
+	"diplom/foundation/web"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"go.uber.org/zap"
+)
+
+// Config contains all the mandatory systems required by handlers.
+type Config struct {
+	Log   *zap.SugaredLogger
+	State *state.State
+	NS    *nameservice.NameService
+	Evts  *events.Events
+}
+
+// Routes binds all the public routes.
+func Routes(app *web.App, cfg Config) {
+	pbl := Handlers{
+		Log:   cfg.Log,
+		State: cfg.State,
+		NS:    cfg.NS,
+		WS:    websocket.Upgrader{},
+		Evts:  cfg.Evts,
+	}
+
+	const version = "v1"
+
+	app.Handle(http.MethodGet, version, "/events", pbl.Events)
+	app.Handle(http.MethodGet, version, "/genesis/list", pbl.Genesis)
+	app.Handle(http.MethodGet, version, "/accounts/list", pbl.Accounts)
+	app.Handle(http.MethodGet, version, "/accounts/list/:account", pbl.Accounts)
+	app.Handle(http.MethodGet, version, "/blocks/list", pbl.BlocksByAccount)
+	app.Handle(http.MethodGet, version, "/blocks/list/:account", pbl.BlocksByAccount)
+	app.Handle(http.MethodGet, version, "/tx/uncommitted/list", pbl.Mempool)
+	app.Handle(http.MethodGet, version, "/tx/uncommitted/list/:account", pbl.Mempool)
+	app.Handle(http.MethodPost, version, "/tx/submit", pbl.SubmitWalletTransaction)
+	app.Handle(http.MethodPost, version, "/tx/proof/:block/", pbl.SubmitWalletTransaction)
+}
---
+++ app/wallet/cli/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "diplom/app/wallet/cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
